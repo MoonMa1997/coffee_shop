35bd494e510c9a62e4ce91dfcd87432189dc8bcc
MoonMa1997 <66760543+MoonMa1997@users.noreply.github.com>
2023-02-09 21:24:54 +0800

    初始化
    文件上传

---
+++ coffeeshop1/src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+// import * as Vue from 'vue' // Vue 3
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import './assets/main.css'
+
+const app = createApp(App)
+
+// const app = Vue.createApp(App)
+app.use(VueAxios, axios)
+
+app.use(router)
+
+app.mount('#app')
